8c72cf530cdba6f4bff1ebfc8155a73f4ebeed65
Lichthagel <hey@lichthagel.de>
2024-10-12 00:00:47 +0200

    refactor: include config files in tsconfig

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,6 @@ export default [
     },
   },
   {
-    ignores: ["dist/**/*", "*.config.js"],
+    ignores: ["dist/**/*"],
   },
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -2,52 +2,49 @@ import path from "node:path";
 import WebpackUserscript from "webpack-userscript";
 
 export default {
-    entry: "./src/index.user.ts",
-    output: {
-        filename: "lichtHikari.user.js",
-        path: path.resolve("./dist"),
-        chunkFormat: "array-push"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            babelrc: false,
-                            presets: [
-                                ["@babel/preset-typescript"]
-                            ]
-                        }
-                    }
-                ],
-                exclude: /node_modules/
+  entry: "./src/index.user.ts",
+  output: {
+    filename: "lichtHikari.user.js",
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    path: path.resolve("./dist"),
+    chunkFormat: "array-push",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              babelrc: false,
+              presets: [["@babel/preset-typescript"]],
             },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".ts", ".js"]
-    },
-    mode: "none",
-    devServer: {
-        port: 9000,
-        client: {
-            webSocketURL: "ws://localhost:9000/ws"
-        },
-        allowedHosts: [
-            'anilist.co'
-        ]
-    },
-    plugins: [
-        new WebpackUserscript({
-            headers: "./meta.json"
-        })
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    target: "es2020"
-}
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  mode: "none",
+  devServer: {
+    port: 9000,
+    client: {
+      webSocketURL: "ws://localhost:9000/ws",
+    },
+    allowedHosts: ["anilist.co"],
+  },
+  plugins: [
+    new WebpackUserscript({
+      headers: "./meta.json",
+    }),
+  ],
+  target: "es2020",
+};
