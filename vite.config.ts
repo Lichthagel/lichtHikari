7f91fc9b7dcc0b67648ba7739d8fccf2d183b621
Lichthagel <hey@lichthagel.de>
2024-10-13 20:41:19 +0200

    refactor(meta): refer to package.json in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
 import icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
-import monkey, { ViolentmonkeyUserScript } from "vite-plugin-monkey";
+import monkey from "vite-plugin-monkey";
 import solidPlugin from "vite-plugin-solid";
 
-import meta from "./meta.json";
+import pkg from "./package.json";
 
 export default defineConfig({
   plugins: [
     solidPlugin(),
     monkey({
       entry: "src/index.user.ts",
-      userscript: meta as ViolentmonkeyUserScript,
+      userscript: {
+        name: pkg.name,
+        namespace: `${pkg.author}/${pkg.name}`,
+        version: pkg.version,
+        description: pkg.description,
+        author: pkg.author,
+        homepageURL: pkg.repository.url.replace("git+", "").replace(".git", ""),
+        supportURL: pkg.bugs.url,
+        downloadURL: "https://lichthagel.github.io/lichtHikari/lichtHikari.user.js",
+        updateURL: "https://lichthagel.github.io/lichtHikari/lichtHikari.user.js",
+        license: pkg.license,
+        match: ["*://anilist.co/*"],
+      },
     }),
     icons({
       compiler: "solid",
